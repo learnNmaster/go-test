1cff919b05af5f6f2d5215a31fd2d2385d36d73a
learnNmaster <jagjot.singh.chhabra@gmail.com>
2024-09-12 23:03:09 +0530

    testing http server

--- src/main.go
+++ src/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Testing out loops")
 
-	for i := 0; i < 10; i++ {
-		fmt.Println("Occurrence: ", i+1)
-		printVal(i)
-	}
+	//for i := 0; i < 10; i++ {
+	//	fmt.Println("Occurrence: ", i+1)
+	//printVal(i)
+	//}
+
+	printVal(1)
+	printVal(3)
+
+	startHttpServer()
 }
 
 func printVal(a int) {
@@ -22,4 +28,14 @@ func printVal(a int) {
 	default:
 		fmt.Println("unsupported operation")
 	}
+}
+
+func startHttpServer() {
+	http.HandleFunc("/", handleReq)
+
+	http.ListenAndServe(":80", nil)
+}
+
+func handleReq(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to my website!")
 }
