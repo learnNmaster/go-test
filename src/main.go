fd7518e05be4f78ad8a72fdd0de429e18095cea6
learnNmaster <jagjot.singh.chhabra@gmail.com>
2024-09-12 22:50:20 +0530

    testing switch case

--- src/main.go
+++ src/main.go
@@ -9,5 +9,17 @@ func main() {
 
 	for i := 0; i < 10; i++ {
 		fmt.Println("Occurrence: ", i+1)
+		printVal(i)
+	}
+}
+
+func printVal(a int) {
+	switch a {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	default:
+		fmt.Println("unsupported operation")
 	}
 }
